14a5920c99ca13eddde81ceb81f3db42a0c9190f
Gonçalo Matos <goncalomatos3@gmail.com>
2017-10-07 19:33:31 +0100

    Start both servers on index

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,6 +1,15 @@
+import {GameHostSocketServer} from './GameHostSocketServer';
 import {PlayerSocketServer} from './PlayerSocketServer';
 
 const playerSocketServer = new PlayerSocketServer();
+const gameHostSocketServer = new GameHostSocketServer({
+    start: () => {
+        playerSocketServer.startQuestion();
+    },
+    stop: () => {
+        playerSocketServer.stopQuestion();
+    },
+});
 
 playerSocketServer.startServer(9191);
-playerSocketServer.startQuestion();
+gameHostSocketServer.startServer(9292);
