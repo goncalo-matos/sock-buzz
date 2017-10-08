0cfb59b46e32238583c95e8547040b82bd099334
Gonçalo Matos <goncalomatos3@gmail.com>
2017-10-09 00:32:46 +0100

    Reorganize code

--- packages/client/src/Game/index.tsx
+++ packages/client/src/Game/index.tsx
@@ -7,12 +7,12 @@ const PLAYER_WEBSOCKET_PATH = 'ws://0.0.0.0:9191';
 class Game extends React.Component {
     private _socketConnection: WebSocket;
 
-    public componentDidMount() {
-        getWebSocket(PLAYER_WEBSOCKET_PATH).then((ws) => this._socketConnection = ws);
-    }
-
     public buzz() {
         this._socketConnection.send('BUZZ');
+    }
+
+    public componentDidMount() {
+        getWebSocket(PLAYER_WEBSOCKET_PATH).then((ws) => this._socketConnection = ws);
     }
 
     public render() {
