10194a6daa65c3ff6feb3824482f5afcbe75706a
Gonçalo Matos <goncalomatos3@gmail.com>
2017-10-09 01:23:18 +0100

    Stop using routing and change to component according to login

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -4,20 +4,54 @@ import {
     Link,
     Route,
     Switch,
+    Redirect
 } from 'react-router-dom';
 
 import { Admin } from './Admin';
 import { Game } from './Game';
+import { Login } from './Login';
 
-class App extends React.Component {
+interface IAppState {
+    hasLoggedIn: boolean;
+    isAdmin: boolean;
+    username: string;
+}
+
+class App extends React.Component<any, IAppState> {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hasLoggedIn: false,
+            isAdmin: false,
+            username: '',
+        };
+    }
+
+    public login(username: string) {
+        let isAdmin = false;
+        // todo implement actual check
+        if (username === 'admin') {
+            isAdmin = true;
+        }
+
+        this.setState({
+            hasLoggedIn: true,
+            isAdmin,
+            username,
+        });
+    }
 
     public render() {
-        return <Router>
-            <Switch>
-                <Route exact path='/' component={Game} />
-                <Route path='/admin' component={Admin} />
-            </Switch>
-        </Router>;
+        if (this.state.hasLoggedIn) {
+            if (this.state.isAdmin) {
+                return <Admin />;
+            }
+
+            return <Game />;
+        } else {
+            return <Login onLogin={(username) => this.login(username)}/>;
+        }
     }
 }
 
