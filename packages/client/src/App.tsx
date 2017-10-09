968842eb9720dce094e3664afc0de446d337b1ac
Gonçalo Matos <goncalomatos3@gmail.com>
2017-10-09 01:26:22 +0100

    Remove unused package

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,11 +1,4 @@
 import * as React from 'react';
-import {
-    BrowserRouter as Router,
-    Link,
-    Route,
-    Switch,
-    Redirect
-} from 'react-router-dom';
 
 import { Admin } from './Admin';
 import { Game } from './Game';
