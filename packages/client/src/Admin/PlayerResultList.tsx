c0df40e67deb06e1427b8208ec6eec392872d72f
Gonçalo Matos <goncalomatos3@gmail.com>
2017-10-18 12:00:26 +0100

    Change player result list

--- packages/client/src/Admin/PlayerResultList.tsx
+++ packages/client/src/Admin/PlayerResultList.tsx
@@ -16,11 +16,23 @@ interface IPlayerResultListProps {
 class PlayerResultList extends React.Component<IPlayerResultListProps, any> {
 
     public render() {
+        let firstTime: Date;
         const listItems = Array.from(this.props.players)
-            .sort(([, playerResultA], [, playerResultB]) => playerResultB.time.getTime() - playerResultA.time.getTime())
-            .map(([key, playerResult]) => <li key={key}>
-                {playerResult.player.name}{playerResult.time.toDateString()}
-            </li>);
+            .sort(([, playerResultA], [, playerResultB]) => playerResultA.time.getTime() - playerResultB.time.getTime())
+            .map(([key, playerResult], index) => {
+                let time;
+
+                if (index === 0 ) {
+                    firstTime = playerResult.time;
+                    time = playerResult.time.toDateString();
+                } else {
+                    time = `+${playerResult.time.getTime() - time / 1000}s`;
+                }
+
+                return <li key={key}>
+                    {playerResult.player.name}{time}
+                </li>;
+            });
 
         return <ul>
             {listItems}
